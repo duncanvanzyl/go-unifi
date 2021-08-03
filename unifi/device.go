f10815f8d35c4b9b7187bd9b7322ebc2c9b063bf
Duncan van Zyl <duncanvanzyl@gmail.com>
2021-08-03 13:34:11 -0400

    add providion to device

--- unifi/device.go
+++ unifi/device.go
@@ -2,6 +2,8 @@ package unifi
 
 import (
 	"context"
+	"fmt"
+	"log"
 )
 
 func (c *Client) ListDevice(ctx context.Context, site string) ([]Device, error) {
@@ -38,4 +40,34 @@ func (c *Client) GetDevice(ctx context.Context, site, id string) (*Device, error
 	}
 
 	return nil, &NotFoundError{}
+}
+
+func (c *Client) ProvisoionDeviceByMAC(ctx context.Context, site, mac string) (interface{}, error) {
+	reqBody := struct {
+		MAC string `json:"mac"`
+		CMD string `json:"cmd"`
+	}{
+		MAC: mac,
+		CMD: "force-provision",
+	}
+
+	// var respBody struct {
+	// 	Meta meta   `json:"meta"`
+	// 	Data []Site `json:"data"`
+	// }
+	respBody := make(map[string]string)
+
+	// POST https://192.168.1.8:8443/api/s/default/cmd/devmgr
+	// {"mac":"b4:fb:e4:27:87:65","cmd":"force-provision"}
+	// err := c.do(ctx, "GET", fmt.Sprintf("s/%s/stat/user/%s", site, mac), nil, &respBody)
+
+	err := c.do(ctx, "POST", fmt.Sprintf("s/%s/default/cmd/devmgr", site), reqBody, &respBody)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Fatalf("%+v", respBody)
+
+	// return respBody.Data, nil
+	return nil, nil
 }
